6629cf93ae7eab0cdcedba98142b4073ed6c532e
Kaique Borges <rique.kaique@gmail.com>
2023-01-18 21:58:39 -0300

    Adds multer and get file metadata

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express")
+var cors = require("cors")
+var multer = require("multer")
+require("dotenv").config()
 
-var app = express();
+var upload = multer().single("upfile")
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+var app = express()
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
+app.use(cors())
+app.use("/public", express.static(process.cwd() + "/public"))
 
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html")
+})
 
+app.post("/api/fileanalyse", upload, (req, res) => {
+  res.send({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  })
+})
 
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
-});
+  console.log("Your app is listening on port " + port)
+})
